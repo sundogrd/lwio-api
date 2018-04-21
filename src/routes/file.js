066b3c8b3d471795742ee25a1d7ef55fab478928
lwyj123 <443474713@qq.com>
2018-04-21 21:03:54 +0800

    modify upload api url

--- src/routes/file.js
+++ src/routes/file.js
@@ -2,10 +2,9 @@ const KoaRouter = require('koa-router')
 
 const controllers = require('../controllers/index.js')
 
-const router = new KoaRouter({ prefix: '/file' })
+const router = new KoaRouter({ prefix: 'api/file' })
 
 router
   .post('/upload', controllers.file.Upload)
-
 
 module.exports = router
