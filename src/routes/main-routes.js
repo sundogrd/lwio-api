ebc2143e58f00920dc776328aea0b135c7a84aac
lwyj123 <443474713@qq.com>
2018-04-21 21:02:05 +0800

    delete main api

--- src/routes/main-routes.js
+++ src/routes/main-routes.js
@@ -10,9 +10,9 @@ router
   })
 
   // .all('/upload', controllers.upload.default)
-  .get('/api/:name', controllers.api.Get)
-  .post('/api/:name', controllers.api.Post)
-  .put('/api/:name', controllers.api.Put)
-  .del('/api/:name', controllers.api.Delect)
+  // .get('/api/:name', controllers.api.Get)
+  // .post('/api/:name', controllers.api.Post)
+  // .put('/api/:name', controllers.api.Put)
+  // .del('/api/:name', controllers.api.Delect)
 
 module.exports = router
